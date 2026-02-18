8bd24d833c04458ee5217c0887757a7dfbc6180c
rozi <rozichilwal09@gmail.com>
2026-02-18 18:59:00 +0530

    fixes inngest.js

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,5 +1,6 @@
 import {Inngest} from "inngest";
 import {connectDB} from "./db.js";
+import User from "../models/User.js";
 
 
 export const inngest=new Inngest({id:"stack-clone"});
@@ -28,6 +29,7 @@ const deleteUserFromDB=inngest.createFunction(
     {id:"delete-user-from-db"},
     {event:"clerk/user.deleted"},
     async({event})=>{
+        await connectDB();
         const {id} =event.data;
         await User.deleteOne({clerkId:id});
         
