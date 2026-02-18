a2a828fe48e77427faa2b58110e05b42bf24cadb
rozi <rozichilwal09@gmail.com>
2026-02-18 18:52:28 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,43 @@
 import express from "express";
 import {ENV} from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import {clerkMiddleware} from "@clerk/express";
+import {inngest} from "./config/inngest.js";
+import {functions} from "./config/inngest.js";
+import {serve} from "inngest/express";
 
 
 const app = express();
 
 
+app.use
+// req.auth will be available in the request object 
+app.use(clerkMiddleware()); 
+
+
+app.use("/api/inngest",serve({client:inngest,functions}));
 
 app.get("/",(req,res)=>{
     res.send("hello from rozi");
 });
 
 
+const stratSever = async()=>{
+    try {
+        await connectDB();
+        if(ENV.NODE_ENV!=="production"){
+            app.listen(ENV.PORT,()=>{
+            console.log(`Server started on port ${ENV.PORT}`);
+            
+         })
+        }
+    } catch (error) {
+        console.error("Error starting server:", error);
+        process.exit(1);
+    }
+}
 
-app.listen(ENV.PORT,()=>{
-    console.log(`Server started on port ${ENV.PORT}`);
-    connectDB();
-})
+stratSever();
+
+export default app;
+
