74d2ed30e3c5bd6bf4cfb50679110bc6d7a1a8c4
rozi <rozichilwal09@gmail.com>
2026-02-19 18:55:09 +0530

    authprovider and tool setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,27 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
-import {BrowserRouter} from 'react-router'
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import * as Sentry from "@sentry/react";
+
+import AuthProvider from './providers/AuthProvider.jsx'
+
+
+
+const queryClient = new QueryClient();
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -12,11 +30,32 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
+
+Sentry.init({
+  dsn: "https://ef49f61b954c1ba9c99361cdf44230c1@o4510905619054592.ingest.us.sentry.io/4510912791576576",
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        <App />
+      <QueryClientProvider client={queryClient}>
+         <AuthProvider>
+          <App />
+         </AuthProvider>
+         <Toaster position="top-right" />
+      </QueryClientProvider>
+       
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
